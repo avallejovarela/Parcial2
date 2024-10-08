a4171aeebd6179018a8aaae4c058640d89f0fd5c
andres vallejo varela <andres4513330@gmail.com>
2024-10-07 22:11:59 -0500

    Clasificar estudiante

--- Parcial2/Parcial2/Program.cs
+++ Parcial2/Parcial2/Program.cs
@@ -9,7 +9,7 @@
         string nombre;
         int cantidadNotas;
         double sumaNotas, nota, promedio;
-        while (continuar) 
+        while (continuar)
         {
             Console.WriteLine("Ingresa la cantidad de estudiantes; ");
             numeroEstudiantes = int.Parse(Console.ReadLine());
@@ -25,13 +25,33 @@
                 cantidadNotas = int.Parse(Console.ReadLine());
 
                 sumaNotas = 0;
-                for (int j = 0; j < cantidadNotas; j++) ;
+                for (int j = 0; j < cantidadNotas; j++)
                 {
                     Console.WriteLine($"Ingrese la nota {j + 1} del estudiante {nombre}:");
                     nota = double.Parse(Console.ReadLine());
                     sumaNotas += nota;
                 }
-                 promedio = sumaNotas / cantidadNotas;
+                promedio = sumaNotas / cantidadNotas;
+
+                // Clasificar estudiante
+                string categoria;
+                if (promedio >= 4.5)
+                {
+                    categoria = "Excelente";
+                }
+                else if (promedio >= 4.0 && promedio <= 4.4)
+                {
+                    categoria = "Sobresaliente";
+                }
+                else if (promedio >= 3.5 && promedio <= 3.9)
+                {
+                    categoria = "Bueno";
+                }
+                else
+                {
+                    categoria = "Insuficiente";
+                }
             }
         }
+    }
 }
