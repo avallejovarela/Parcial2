9ea003c94289ef5d325f96137bb8df0ca286a20a
andres vallejo varela <andres4513330@gmail.com>
2024-10-07 21:58:08 -0500

    informacion del estudiantes y se agregaron nuevas variables

--- Parcial2/Parcial2/Program.cs
+++ Parcial2/Parcial2/Program.cs
@@ -6,10 +6,22 @@
         // variables de entrada
         bool continuar = true;
         int numeroEstudiantes;
-
+        string nombre;
+        int cantidadNotas
         while (continuar) 
         {
             Console.WriteLine("Ingresa la cantidad de estudiantes; ");
             numeroEstudiantes = int.Parse(Console.ReadLine());
-    }
+
+            int ganaron = 0, perdieron = 0;
+
+            for (int i = 0; i < numeroEstudiantes; i++)
+            {
+                Console.WriteLine($"Ingrese el nombre del estudiante {i + 1}:");
+                nombre = Console.ReadLine();
+
+                Console.WriteLine($"Ingrese la cantidad de notas del estudiante {nombre}:");
+                cantidadNotas = int.Parse(Console.ReadLine());
+            }
+        }
 }
