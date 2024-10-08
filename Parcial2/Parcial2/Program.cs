f248999f27b37b9938ba3f38147d6b12dd8bbd42
andres vallejo varela <andres4513330@gmail.com>
2024-10-07 20:56:30 -0500

    variables de entrada

--- Parcial2/Parcial2/Program.cs
+++ Parcial2/Parcial2/Program.cs
@@ -3,5 +3,9 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("prueba2");
+
+        //variables de entrada
+        int n;
+        bool jugarNuevamente = true;
     }
 }
